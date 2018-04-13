578b1ea469bb9efd23c7e8c892cf150858998225
Kee-Won Lee <kwlee@hallym.ac.kr>
2018-04-13 20:16:50 +0900

    Colors added Please enter the commit message for your changes. Lines starting

--- R/horsekick_plot.R
+++ R/horsekick_plot.R
@@ -1,10 +1,20 @@
-options(digits = 3)
-plot(n_deaths, horsekick_cum, 
-     xlim = c(-0.5, 4.5), ylim = c(50, 100), 
+plot(x = n_deaths, y = horsekick_cum, 
+     xlim = c(-0.5,4.5), ylim = c(50, 100), 
      xlab = "사망자 수", ylab = "누적 백분률(%)", 
      yaxt = "n")
-axis(side = 2, 
+axis(side = 2,
      at = c(50, horsekick_cum), 
-     labels = format(c(50, horsekick_cum), nsmall = 1),
+     labels = format(c("", horsekick_cum[1:4], ""), nsmall = 1),
      las = 2)
-abline(v = 0:4, lty = 3)
+axis(side = 2,
+     at = c(48.5, horsekick_cum[5] + 1.5), 
+     tick = FALSE,
+     labels = format(c(50, horsekick_cum[5]), nsmall = 1),
+     las = 2)
+abline(v = 0:4, 
+       lty = 3)
+lines(c(0, 1), rep(horsekick_cum[1], 2), lty = 1, lwd = 2)
+lines(c(1, 2), rep(horsekick_cum[2], 2), lty = 1, lwd = 2)
+lines(c(2, 3), rep(horsekick_cum[3], 2), lty = 1, lwd = 2)
+lines(c(3, 4), rep(horsekick_cum[4], 2), lty = 1, lwd = 2)
+lines(c(4, 5), rep(horsekick_cum[5], 2), lty = 1, lwd = 2)
